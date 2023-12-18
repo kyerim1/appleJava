13b850a409d1ad227f95da2bee71aca991e060c5
kyerim1 <nilzero25@gmail.com>
2023-12-18 10:27:55 +0900

    자바배열

--- study/src/study121801/main_class.java
+++ study/src/study121801/main_class.java
@@ -3,8 +3,19 @@ package study121801;
 public class main_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		// 자바스크립트 - let a = new Array();
+		//             let b = [10,20,30,40];
+		//       a.push(3123);   b.push(233);
+		
+		int[] arr1 = {101,20};
+		
+		System.out.println( arr1[0] );
 	}
 
 }
+
+
+
+
+
