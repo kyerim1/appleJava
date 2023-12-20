687f79c1b958c39abffb6aaaf1bf4cc6aac85175
kyerim1 <nilzero25@gmail.com>
2023-12-20 13:43:24 +0900

    12월 20일

--- study/src/study121901/String_test.java
+++ study/src/study121901/String_test.java
@@ -1,7 +1,8 @@
 package study121901;
 
 public class String_test {
-
+public static final String blue="\u001B[34m";
+public static final String end = "\u001B[0m";
 	public static void main(String[] args) {
 		
 		//
@@ -26,7 +27,23 @@ public class String_test {
 				+"트리 아래의 선물 같은것도 신경쓰지 않아요 # 당신을 원해요 #"
 				+"당신이 생각하는 것보다 더 # 내 소원을 이뤄줘요 #"
 				+ "내가 크리스마스에 원하는건 당신이에요";
-		System.out.println( song );
+		System.out.println("원본 :" + song +"\n" );
+		
+		String song1 = song.replaceAll("#", "\n");
+		System.out.println( song1 );
+		String[] song2 = song.split("#");
+		for( String sng : song2)
+			System.out.println(sng);
+		
+		String song3 = song1.replaceAll("당신", "그대");
+		System.out.println("\n\n"+song3+"\n");
+		
+		String song4 = song1.replaceAll("크리스마스", "christmas");
+		System.out.println(song4.toUpperCase()+ "\n\n");
+		
+		String[] song5 = song.split("#");
+		System.out.println( song5[3].trim()  );
+		
 /*
  *   1. # 기준으로 한줄씩 출력 하세요( #은  출력창에 나오면 안되요)
  *   2. 당신 이라는 단어 대신  "그대" 라고 바꾸어서 출력하세요
