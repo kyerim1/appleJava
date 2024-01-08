352548141320c0ba80acf21e050b244642c4af2c
kyerim1 <nilzero25@gmail.com>
2024-01-08 17:24:49 +0900

    1월 8일

--- java_collection/src/cafe/Order.java
+++ java_collection/src/cafe/Order.java
@@ -1,6 +1,13 @@
 package cafe;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -16,15 +23,107 @@ public class Order {
 		Category main = Category.커피;
 		while(true) {
 			System.out.println("1.커피  2.음료  3.빽스치노");
-			System.out.println( cafeMenu.get(main) );
-			scan.nextLine();
+			Set<menu> list = cafeMenu.get(main);
+			Iterator<menu> tmp = list.iterator();
+			while( tmp.hasNext() ) {
+				System.out.println(  tmp.next() );
+			}
+			System.out.print( "주문 메뉴 선택 및 항목 선택 : ");
+			String input = scan.nextLine();
+			// 메뉴를 입력했는가? , 분류 번호를 입력했는가?
+			if( isInteger( input )  ) // 정수변환 가능- 분류번호입력
+				main = SelectCategory( file.i(input) );
+			else // 메뉴를 입력
+			{	
+				progress(input);
+				break;
+			}
 		}
 		
 	}
 	
 	// 주문 진행하는 메서드 : 주문할 제품을 선택했다면 결제 까지 진행 되는 메서드 
-	
+	private static void progress(String orderMenu) {
+		if(  Order_Counter.Menu.containsKey(orderMenu)  ) {//내가선택한 메뉴가 map의 키값인가?
+		
+			menu select = Order_Counter.Menu.get(orderMenu); //내가 선택한 메뉴의 menu클래스객체
+			//핫,아이스?   추가 선택 유도 
+			int price=0;
+			if( select.getCost1() !=0 && select.getCost2() !=0 ) {
+				System.out.print("1.HOT  2.ICE : ");
+				int HI = scan.nextInt();
+				price = HI==1? select.getCost1() : select.getCost2();
+			}else {
+				price = select.getCost1() !=0 ? select.getCost1() : select.getCost2();
+			}
+			DecimalFormat df = new DecimalFormat("\u20A9###,###원");
+			System.out.println("\n===== 주문 내역 ==== \n");
+			System.out.println( select  );
+			System.out.printf("카페인 : %dmg , 칼로리 : %dkcal , 용량 : %dml \n",
+					select.getCaffeine(), select.getCalorie(), select.getMl() );
+			System.out.println(" 결제금액 : "+df.format(price));
+			
+			save_history( select.getMenu_name(), price );
+		}
+		// 내가 선택한 메뉴가 map의 키값이 아니라면  다시 메뉴를 선택하게 해줘야한다.
+	}
 	
 	
 	//주문 내역을 Order_history를 통해 저장 메서드
+	private static void save_history(String menu, int price) {
+		String id = Order_Counter.user.getId();
+		
+		String url = "jdbc:mysql://localhost:3306/javaTest";
+		String user="javaTest";
+		String password="123456";
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		}catch( ClassNotFoundException e) {
+			System.out.println(" 드라이버 로드실패");
+		}
+		
+		Connection conn=null; // mysql접속 상태값 저장
+		ResultSet set=null; // mysql로부터 데이터를 받는다.
+		PreparedStatement pt=null; // mysql에 query문 전달
+		try {
+			conn = DriverManager.getConnection(url,user,password);
+			System.out.println("접속 성공");
+		}catch(SQLException e) {
+			System.out.println("접속 실패");
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	private static Category SelectCategory(int num) {
+		switch(num) {
+			case 1: return Category.커피;
+			case 2: return Category.음료;
+			case 3: return Category.빽스치노;
+		}
+		return Category.커피;
+	}
+	
+	private static boolean isInteger(String str) {
+		try {
+			Integer.parseInt(str);
+			return true;
+		}catch(NumberFormatException e) {
+			return false;
+		}
+		
+	}
+	
+	
 }
+
+
+
+
+
--- java_collection/src/customer/Order_History.java
+++ java_collection/src/customer/Order_History.java
@@ -1,5 +1,51 @@
 package customer;
 
 public class Order_History {
-
+	
+	private int num;
+	private String customer;
+	private String menu;
+	private int price;
+	
+	public Order_History(int num, String customer, String menu, int price) {
+		this.customer=customer;
+		this.menu=menu;
+		this.price=price;
+		this.num=num;
+	}
+	
+	
+	@Override 
+	public String toString() {
+		
+		return "고객 : "+customer+" 메뉴 : "+menu+" 금액 : "+price;
+	}
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
 }
